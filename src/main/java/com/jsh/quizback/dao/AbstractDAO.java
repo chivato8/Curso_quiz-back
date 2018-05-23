84ef1aeff78fe60fa04f9473a4fa74b855cdbe8e
jsevillanoh <juan.sevillano.hernandez@gmail.com>
2018-05-23 13:32:29 +0200

    DAO Abstracto Eliminado.

--- src/main/java/com/jsh/quizback/dao/AbstractDAO.java
+++
@@ -1,7 +0,0 @@
-package com.jsh.quizback.dao;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface AbstractDAO<T> extends PagingAndSortingRepository<T, Integer> {
-
-}
