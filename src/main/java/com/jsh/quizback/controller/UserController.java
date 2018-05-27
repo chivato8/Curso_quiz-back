93ef5e7ab59a8e82d171a2773e5edf10b7d7d3ff
jsevillanoh <juan.sevillano.hernandez@gmail.com>
2018-05-27 19:18:41 +0200

    UserController creado con todos los CRUD

---
+++ src/main/java/com/jsh/quizback/controller/UserController.java
@@ -0,0 +1,98 @@
+package com.jsh.quizback.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jsh.quizback.dto.ConfirDTO;
+import com.jsh.quizback.dto.UserDTO;
+import com.jsh.quizback.exception.NotFoundException;
+import com.jsh.quizback.model.User;
+import com.jsh.quizback.service.MapperService;
+import com.jsh.quizback.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping(value="/user")
+public class UserController {
+	
+	@Autowired
+	UserService userservice;
+	
+	@Autowired
+	MapperService mp;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<UserDTO> findAll(@RequestParam(required=false, defaultValue="0") Integer page, @RequestParam(required=false, defaultValue="5") Integer size){
+		log.info("Recuperando toda la lista de Usuarios");
+		return userservice.findAll(page,size);		
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{idUser}")
+	public UserDTO findOneByIdUser(@PathVariable Integer idUser) throws NotFoundException {
+		log.info("Recuperando Usuario con Id = " + idUser);
+		return userservice.findByIdUser(idUser);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/nameUser={nameUser}")
+	public List<UserDTO> findByName(@PathVariable String nameUser) throws NotFoundException{
+		log.info("Recuperando Usuario con Nombre = " + nameUser);
+		return userservice.findByName(nameUser);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/emailUser={emailUser}")
+	public UserDTO findByEmailUser(@PathVariable String emailUser)throws NotFoundException{
+		log.info("Recuperando Usuario con Email = " + emailUser);
+		return userservice.findByEmailUser(emailUser);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/idCourse={idCourse}")
+	public List<UserDTO> findByIdCourseUser(Integer idCourse)throws NotFoundException{
+		log.info("Recuperando Usuario por IdCourse = " + idCourse);
+		return userservice.findByIdCourseUser(idCourse);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public UserDTO create(@RequestBody UserDTO u) {
+		log.info("Creando Usuario");
+		log.info("Nombre: "+u.getNameUser());
+		log.info("Email: "+u.getEmailUser());
+		log.info("Password: "+u.getPasswordUser());
+		final User user = mp.map(u);
+		final User createUser = userservice.create(user);
+		log.info("Creando Usuario = " + u.getNameUser());
+		return mp.map(createUser);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/{idUser}")
+	public UserDTO update(@PathVariable Integer idUser, @RequestBody UserDTO u)throws NotFoundException {
+		log.info("Modificando Usuario con ID: "+idUser);
+		log.info("Nombre: "+u.getNameUser());
+		log.info("Email: "+u.getEmailUser());
+		log.info("Password: "+u.getPasswordUser());
+		u.setIdUser(idUser);
+		final User user = mp.map(u);
+		user.setIdUser(idUser);
+		userservice.update(user);
+		return userservice.findByIdUser(idUser);
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{idUser}")
+	public ConfirDTO delete(@PathVariable Integer idUser) throws NotFoundException {
+		log.info("Borrando Usuario con Id = " + idUser);
+		userservice.delete(idUser);
+		final ConfirDTO confirdto = new ConfirDTO();
+		confirdto.setCode("OK");
+		confirdto.setText("Borrado Correctamente");
+		return confirdto;
+	}	
+}
