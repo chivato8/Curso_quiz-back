aabed4914b28aff67dc2e706d3d06682d7385c3d
jsevillanoh <juan.sevillano.hernandez@gmail.com>
2018-05-27 19:19:57 +0200

    Excepciones creadas.

--- src/main/java/com/jsh/quizback/exception/InvalidDataException.java
+++ src/main/java/com/jsh/quizback/exception/InvalidDataException.java
@@ -1,19 +1,27 @@
 package com.jsh.quizback.exception;
 
+import lombok.Getter;
+
+@Getter
 public class InvalidDataException extends Exception {
 
 	private static final long serialVersionUID = 7540690592995095588L;
 
-	private static final String MSG = "Los Datos NO son Validos.";
-	
-	public InvalidDataException(String message)
-	{
-		super(message);
-	}
+	private static final String MSG = "El Valor Introducido no Encontrado: ";
 	
 	public InvalidDataException()
 	{
 		super(MSG);
+	}
+	
+	public InvalidDataException(final Integer message)
+	{
+		super(MSG+message);
+	}
+	
+	public InvalidDataException(final String message)
+	{
+		super(MSG+message);
 	}
 
 }
--- src/main/java/com/jsh/quizback/exception/NotFoundException.java
+++ src/main/java/com/jsh/quizback/exception/NotFoundException.java
@@ -1,19 +1,28 @@
 package com.jsh.quizback.exception;
 
+import lombok.Getter;
+
+@Getter
 public class NotFoundException extends Exception{
 
 	private static final long serialVersionUID = -7240394839863594491L;
 	
-	private static final String MSG = "La Entidad no Existe.";
-	
-	public NotFoundException(String message)
-	{
-		super(message);
-	}
+	private static final String MSG = "La Entidad no Existe. ";
 	
 	public NotFoundException()
 	{
 		super(MSG);
 	}
+	
+	public NotFoundException(final Integer message)
+	{
+		super(MSG+message);
+	}
+	
+	public NotFoundException(final String message)
+	{
+		super(MSG+message);
+	}
+	
 	
 }
