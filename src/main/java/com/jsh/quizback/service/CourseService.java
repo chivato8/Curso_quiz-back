d95cf0e28e75a292b8a5e74f2264d8fbc472552a
jsevillanoh <juan.sevillano.hernandez@gmail.com>
2018-05-31 23:17:31 +0200

    Creación de Cursos. (Alumnos y Cuestionario). Insertar los alumnos y el cuestionario de dicho curso.

--- src/main/java/com/jsh/quizback/service/CourseService.java
+++ src/main/java/com/jsh/quizback/service/CourseService.java
@@ -1,5 +1,18 @@
 package com.jsh.quizback.service;
 
+import java.util.List;
+
+import com.jsh.quizback.dto.ConfirDTO;
+import com.jsh.quizback.dto.CourseDTO;
+import com.jsh.quizback.exception.NotFoundException;
+
 public interface CourseService {
 
+	public List<CourseDTO> findAll(Integer page, Integer size);
+	
+	public CourseDTO findByIdCourse(Integer idCourse) throws NotFoundException;
+	
+	public List<CourseDTO> findByIdUserCourse(Integer idCourse) throws NotFoundException;
+	
+	public ConfirDTO saveCourseUser(Integer idCourse, Integer idUser) throws NotFoundException;
 }
