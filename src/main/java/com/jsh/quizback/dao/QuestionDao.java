ff8cb1f7e4e30737e61ee17149d7d96d998b501e
jsevillanoh <juan.sevillano.hernandez@gmail.com>
2018-05-31 23:16:03 +0200

    Mejoras y Correcciones

--- src/main/java/com/jsh/quizback/dao/QuestionDao.java
+++ src/main/java/com/jsh/quizback/dao/QuestionDao.java
@@ -2,10 +2,12 @@ package com.jsh.quizback.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jsh.quizback.model.Question;
 
@@ -64,5 +66,10 @@ public interface QuestionDao extends PagingAndSortingRepository<Question,Integer
 			+ "JOIN QUESTION_QUIZ on q.id_Question = QUESTION_QUIZ.id_Question "
 			+ "WHERE QUESTION_QUIZ.id_Quiz = ?1", nativeQuery=true)
 	public List<Question> findByIdQuizQuestion(@Param(value = "idQuiz") Integer idQuiz);
+	
+	@Modifying
+	@Query(value = "INSERT INTO QUESTION_QUIZ (id_quiz,id_question) VALUES (?1,?2)", nativeQuery = true)
+	@Transactional
+	public void saveQuestionQuiz(@Param("idQuiz") Integer idQuiz, @Param("idQuestion") Integer idQuestion);
 	
 }
