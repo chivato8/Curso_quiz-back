a5bad9d7b2a09a2577fe448f4eea2a550bd4a9bb
jsevillanoh <juan.sevillano.hernandez@gmail.com>
2018-05-31 23:17:40 +0200

    Recuperación resultado por Curso.

--- src/main/java/com/jsh/quizback/service/ResultService.java
+++ src/main/java/com/jsh/quizback/service/ResultService.java
@@ -1,5 +1,16 @@
 package com.jsh.quizback.service;
 
+import java.util.List;
+
+import com.jsh.quizback.dto.ResultDTO;
+import com.jsh.quizback.exception.NotFoundException;
+
 public interface ResultService {
 
+	public List<ResultDTO> findAll(Integer page, Integer size);
+	
+	public List<ResultDTO> findByIdQuiz(Integer idQuiz);
+	
+	public List<ResultDTO> findByResultCourse(Integer idCourse)throws NotFoundException;
+	
 }
