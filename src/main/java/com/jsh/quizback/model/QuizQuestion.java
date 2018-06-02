2660f7281e38ee1925b0bdcaad4fd196cf3255d9
jsevillanoh <juan.sevillano.hernandez@gmail.com>
2018-06-02 22:18:38 +0200

    Creando Base de Datos para almacenar cuestionarios, preguntas y respuestas.

---
+++ src/main/java/com/jsh/quizback/model/QuizQuestion.java
@@ -0,0 +1,40 @@
+package com.jsh.quizback.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class QuizQuestion implements Serializable {
+	
+	private static final long serialVersionUID = -6165984437217430478L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer idQuizQuestion;
+	
+	@Column(nullable = false)
+	Integer idQuiz;
+	
+	@Column(nullable = false)
+	Integer idQuestion;
+	
+	@Column(nullable = false)
+	String textquestion;
+	
+	@Column(nullable = false)
+	String textanswer;
+	
+	@Column(nullable = false)
+	String correct;
+	
+}
